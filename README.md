cafa666f9d5dff54a6617d84a6cd018fcf991ea3
ral0s <ralosgit@gmail.com>
2024-05-21 18:52:22 -0400

    frida

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frida
+
+Dynamic instrumentation toolkit for developers, reverse-engineers, and security researchers.
---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -e
+
+show_usage() {
+  echo "Usage: $(basename $0) takes exactly 1 argument (install | uninstall)"
+}
+
+if [ $# -ne 1 ]
+then
+  show_usage
+  exit 1
+fi
+
+check_env() {
+  if [[ -z "${RALPM_TMP_DIR}" ]]; then
+    echo "RALPM_TMP_DIR is not set"
+    exit 1
+  
+  elif [[ -z "${RALPM_PKG_INSTALL_DIR}" ]]; then
+    echo "RALPM_PKG_INSTALL_DIR is not set"
+    exit 1
+  
+  elif [[ -z "${RALPM_PKG_BIN_DIR}" ]]; then
+    echo "RALPM_PKG_BIN_DIR is not set"
+    exit 1
+  fi
+}
+
+install() {
+  wget https://github.com/indygreg/python-build-standalone/releases/download/20220802/cpython-3.9.13+20220802-x86_64-unknown-linux-gnu-install_only.tar.gz -O $RALPM_TMP_DIR/cpython-3.9.13.tar.gz
+  tar xf $RALPM_TMP_DIR/cpython-3.9.13.tar.gz -C $RALPM_PKG_INSTALL_DIR
+  $RALPM_PKG_INSTALL_DIR/python/bin/pip3.9 install frida==16.0.2 frida-tools==12.0.1
+  ln -s $RALPM_PKG_INSTALL_DIR/python/bin/frida* $RALPM_PKG_BIN_DIR/
+}
+
+uninstall() {
+  rm -rf $RALPM_PKG_BIN_DIR/python
+  rm $RALPM_PKG_BIN_DIR/frida*
+}
+
+run() {
+  if [[ "$1" == "install" ]]; then 
+    install
+  elif [[ "$1" == "uninstall" ]]; then 
+    uninstall
+  else
+    show_usage
+  fi
+}
+
+check_env
+run $1
